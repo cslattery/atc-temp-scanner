a7e3dac2775723eb1348bb495aa9ce460760c329
cslattery <18449477+cslattery@users.noreply.github.com>
2022-07-27 13:03:08 +0100

    add windows and write to file

--- beam_pipeline.py
+++ beam_pipeline.py
@@ -1,22 +1,31 @@
 import ast
 import argparse
+from curses import window
+from datetime import datetime, timezone
+from importlib.resources import path
+from itertools import accumulate
 import json
 import logging
-from re import S
+#from re import S
 from sys import argv
 import yaml
 import time
 import apache_beam as beam
+from apache_beam import window
+from apache_beam.io import fileio
 from google.cloud import pubsub_v1
 
 # write raw data out to file
-# write transformed data to db
+# sliding window to give 5/15/30/60 min average
+# fixed window to create historical averages
 # create aggregations and write to db
 
 
+# add try-except to catch failure to parse advertisement
 class ExtractData(beam.DoFn):
     def process(self, element):
         record = ast.literal_eval(element)
+        record["utc"] = str(datetime.fromtimestamp(float(record["epoch"]/1000), timezone.utc))
         record["temperature"] = (record["advertisement"][6]
                                  + record["advertisement"][7])/10
         record["humidity"] = record["advertisement"][8]
@@ -26,6 +35,7 @@ class ExtractData(beam.DoFn):
         yield json_record
 
 
+# add try-except to catch non-joins
 class AddLocation(beam.DoFn):
     def process(self, element, locations):
         record = ast.literal_eval(element)
@@ -47,11 +57,9 @@ def run(argv=None):
         except yaml.YAMLError as exc:
             logging.ERROR(exc)
 
-    TOPIC = config["pubsub_emulator"]["topic_name"]
     EMULATOR_PROJECT = config["pubsub_emulator"]["project_id"]
     SUBSCRIPTION = config["pubsub_emulator"]["subscription"]
     LOCATION = config["location"]
-    logging.info(type(LOCATION))
 
     subscriber = pubsub_v1.SubscriberClient()
     subscription_path = subscriber.subscription_path(EMULATOR_PROJECT,
@@ -63,7 +71,7 @@ def run(argv=None):
         records = (
             p
             | "Read from sub" >> beam.io.ReadFromPubSub(
-                subscription=subscription_path)
+                subscription=subscription_path, timestamp_attribute='epoch')
             | "Decode" >> beam.Map(lambda m: m.decode("utf-8"))
             | "To .." >> beam.ToString.Element()
         )
@@ -73,10 +81,33 @@ def run(argv=None):
             | "Extract advertisement" >> beam.ParDo(ExtractData())
             | "Add location" >> beam.ParDo(AddLocation(),
                                            locations=LOCATION)
+        )        
+
+        keyval = enh_records | 'Key Val pair' >> beam.Map(lambda row: (row["devicename"], 
+                                         float(row["temperature"])))
+        raw = (
+            enh_records
+            | "to json string" >> beam.Map(lambda row: json.loads(json.dumps(str(row))))
+            | "Window raw data" >> beam.WindowInto(window.FixedWindows(60 * 5))
+            | 'Group elements into windows' >> beam.Reshuffle()
+           )
+
+        live_temp_5min = (
+            keyval 
+            | '5 min window' >> beam.WindowInto(window.SlidingWindows(5*60, 60))
+            | "5 min average" >> beam.combiners.Mean.PerKey()
         )
 
-        enh_records | beam.Map(print)
-        # records | "Write" >> beam.io.WriteToText(args.output, file_name_suffix='.csv')
+        live_temp_15min = (
+            keyval 
+            | '15 min window' >> beam.WindowInto(window.SlidingWindows(15*60, 60))
+            | "15 min average" >> beam.combiners.Mean.PerKey()
+        )
+
+        #enh_records | "live" >> beam.Map(print)
+        live_temp_5min | "5min" >> beam.Map(print)
+        live_temp_15min | "15min" >> beam.Map(print) 
+        raw | "write raw to file" >> beam.io.fileio.WriteToFiles(path='outtie', shards=2, max_writers_per_bundle=1)
 
 
 if __name__ == '__main__':
